5390165becc20dbf3e0221270ab6f34e156be9d5
Daniil <daniil@MacBook-Pro-Daniil.local>
2023-08-14 18:25:12 +0300

    solve first

--- 1/1.go
+++ 1/1.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	arr := []int{5, 3, 6, 1}
+
+	bubbleSort(arr)
+}
+
+func bubbleSort(arr []int) {
+	for i := 0; i < len(arr)-1; i++ {
+		for j := i + 1; j < len(arr); j++ {
+			if arr[i] > arr[j] {
+				arr[i], arr[j] = arr[j], arr[i]
+			}
+		}
+	}
+	fmt.Println(arr)
 }
