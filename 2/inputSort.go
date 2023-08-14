e962b3b3a215593968933ac3d9667b3f617338f5
Daniil <daniil@MacBook-Pro-Daniil.local>
2023-08-14 18:57:25 +0300

    solve second

--- 2/inputSort.go
+++ 2/inputSort.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	arr := []int{5, 3, 6, 1, 2}
+	inputSort(arr)
+	fmt.Println(arr)
+}
+
+func inputSort(arr []int) {
+	for i := 0; i < len(arr); i++ {
+		minIdx := i
+		for j := i; j < len(arr); j++ {
+			if arr[minIdx] > arr[j] {
+				minIdx = j
+			}
+		}
+		arr[minIdx], arr[i] = arr[i], arr[minIdx]
+	}
 }
